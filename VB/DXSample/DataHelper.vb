1cf4255758b571c09ae6d60abfd0256704e5a73a
DevExpressExampleBot <fakeEmail>
2022-12-17 07:00:17 +0400

    Source auto update [skip ci]

--- VB/DXSample/DataHelper.vb
+++ VB/DXSample/DataHelper.vb
@@ -1,7 +1,4 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
+Imports System
 Imports System.Collections.ObjectModel
 Imports System.Windows.Media
 Imports System.Windows.Media.Imaging
@@ -9,58 +6,53 @@ Imports System.Drawing
 Imports System.ComponentModel
 
 Namespace DXSample
-	Public NotInheritable Class DataHelper
 
-		Private Sub New()
-		End Sub
+    Public Module DataHelper
 
-		Public Shared Function GetData1() As ObservableCollection(Of ExampleObject)
-			Dim collection As New ObservableCollection(Of ExampleObject)() From { _
-				New ExampleObject With {.Name = "Basket", .ImageUri ="Images/Basket.png"}, _
-				New ExampleObject With {.Name = "Check", .ImageUri ="Images/Check.png"}, _
-				New ExampleObject With {.Name = "Customer", .ImageUri ="Images/Customer.png"} _
-			}
-			Return collection
-		End Function
-		Public Shared Function GetData2() As ObservableCollection(Of ExampleObject)
-			Dim collection As New ObservableCollection(Of ExampleObject)() From { _
-				New ExampleObject With {.Name = "Folder", .ImageUri ="Images/Folder.png"}, _
-				New ExampleObject With {.Name = "Key", .ImageUri ="Images/Key.png"}, _
-				New ExampleObject With {.Name = "Home", .ImageUri ="Images/Home.png"} _
-			}
-			Return collection
-		End Function
-	End Class
-	Public Class ExampleObject
-		Inherits DevExpress.Mvvm.BindableBase
+        Public Function GetData1() As ObservableCollection(Of ExampleObject)
+            Dim collection As ObservableCollection(Of ExampleObject) = New ObservableCollection(Of ExampleObject)() From {New ExampleObject With {.Name = "Basket", .ImageUri = "Images/Basket.png"}, New ExampleObject With {.Name = "Check", .ImageUri = "Images/Check.png"}, New ExampleObject With {.Name = "Customer", .ImageUri = "Images/Customer.png"}}
+            Return collection
+        End Function
 
-		' Fields...
-		Private _ImageUri As String
-		Private _Name As String
+        Public Function GetData2() As ObservableCollection(Of ExampleObject)
+            Dim collection As ObservableCollection(Of ExampleObject) = New ObservableCollection(Of ExampleObject)() From {New ExampleObject With {.Name = "Folder", .ImageUri = "Images/Folder.png"}, New ExampleObject With {.Name = "Key", .ImageUri = "Images/Key.png"}, New ExampleObject With {.Name = "Home", .ImageUri = "Images/Home.png"}}
+            Return collection
+        End Function
+    End Module
 
-		Public Property Name() As String
-			Get
-				Return _Name
-			End Get
-			Set(ByVal value As String)
-				SetProperty(_Name, value, Function() Name)
-			End Set
-		End Property
+    Public Class ExampleObject
+        Inherits DevExpress.Mvvm.BindableBase
 
-		Public Property ImageUri() As String
-			Get
-				Return _ImageUri
-			End Get
-			Set(ByVal value As String)
-				SetProperty(_ImageUri, value, Function() ImageUri)
-				RaisePropertiesChanged("ImageSource")
-			End Set
-		End Property
+        ' Fields...
+        Private _ImageUri As String
 
-		Public ReadOnly Property ImageSource() As ImageSource
-			Get
-				Return If(String.IsNullOrEmpty(ImageUri), Nothing, New BitmapImage(New Uri("/DXSample;component/" & ImageUri, UriKind.Relative)))
-			End Get
-		End Property
-	End Class
+        Private _Name As String
+
+        Public Property Name As String
+            Get
+                Return _Name
+            End Get
+
+            Set(ByVal value As String)
+                SetProperty(_Name, value, Function() Name)
+            End Set
+        End Property
+
+        Public Property ImageUri As String
+            Get
+                Return _ImageUri
+            End Get
+
+            Set(ByVal value As String)
+                SetProperty(_ImageUri, value, Function() ImageUri)
+                RaisePropertiesChanged("ImageSource")
+            End Set
+        End Property
+
+        Public ReadOnly Property ImageSource As ImageSource
+            Get
+                Return If(String.IsNullOrEmpty(ImageUri), Nothing, New BitmapImage(New Uri("/DXSample;component/" & ImageUri, UriKind.Relative)))
+            End Get
+        End Property
+    End Class
 End Namespace
--- VB/DXSample/MainWindow.xaml.vb
+++ VB/DXSample/MainWindow.xaml.vb
@@ -1,138 +1,119 @@
-﻿Imports System
-Imports System.Linq
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 Imports System.Windows.Input
-Imports System.Collections.Generic
 Imports DevExpress.Xpf.Core.Native
 Imports DevExpress.Xpf.LayoutControl
 Imports System.Collections.ObjectModel
 
 Namespace DXSample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Private handler As Integer = GridControl.InvalidRowHandle
-		Private point As Point
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			gridControl1.ItemsSource = DataHelper.GetData1()
-			tileLayoutControl1.ItemsSource = DataHelper.GetData2()
-		End Sub
+        Private handler As Integer = DataControlBase.InvalidRowHandle
 
-		Private Sub SetToZero()
-			handler = GridControl.InvalidRowHandle
-			point = New Point(-1000, -1000)
-		End Sub
-		#Region "GridToTilelayout"
+        Private point As Point
 
-		Private Sub gridControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.gridControl1.ItemsSource = GetData1()
+            Me.tileLayoutControl1.ItemsSource = GetData2()
+        End Sub
 
+        Private Sub SetToZero()
+            handler = DataControlBase.InvalidRowHandle
+            point = New Point(-1000, -1000)
+        End Sub
 
-			Dim grid As GridControl = TryCast(sender, GridControl)
-			Dim view As GridViewBase = TryCast(grid.View, GridViewBase)
-			Dim mousePos As Point = e.GetPosition(Nothing)
+'#Region "GridToTilelayout"
+        Private Sub gridControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Dim grid As GridControl = TryCast(sender, GridControl)
+            Dim view As GridViewBase = TryCast(grid.View, GridViewBase)
+            Dim mousePos As Point = e.GetPosition(Nothing)
+            If e.LeftButton <> MouseButtonState.Pressed Then Return
+            If handler = DataControlBase.InvalidRowHandle Then Return
+            Dim rect As Rect = New Rect(point.X - SystemParameters.MinimumHorizontalDragDistance, (point.Y - CInt(SystemParameters.MinimumVerticalDragDistance)), SystemParameters.MinimumHorizontalDragDistance * 2, SystemParameters.MinimumVerticalDragDistance * 2)
+            If Not rect.Contains(mousePos) Then
+                Dim dragData As DataObject = New DataObject("GridToLC", TryCast(view.GetRowElementByRowHandle(handler).DataContext, RowData).Row)
+                SetToZero()
+                DragDrop.DoDragDrop(grid, dragData, DragDropEffects.Copy)
+            End If
+        End Sub
 
-			If e.LeftButton <> MouseButtonState.Pressed Then
-				Return
-			End If
-			If handler = GridControl.InvalidRowHandle Then
-				Return
-			End If
+        Private Sub tileLayoutControl1_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
+            If e.Data.GetDataPresent("GridToLC") Then
+                Dim obj As ExampleObject = TryCast(e.Data.GetData("GridToLC"), ExampleObject)
+                Dim tileLayout As TileLayoutControl = TryCast(sender, TileLayoutControl)
+                TryCast(tileLayout.ItemsSource, ObservableCollection(Of ExampleObject)).Add(obj)
+            End If
+        End Sub
 
-			Dim rect As New Rect((point.X - SystemParameters.MinimumHorizontalDragDistance), (point.Y - CInt(Math.Truncate(SystemParameters.MinimumVerticalDragDistance))), SystemParameters.MinimumHorizontalDragDistance * 2, SystemParameters.MinimumVerticalDragDistance * 2)
+        Private Sub gridControl1_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            If e.LeftButton = MouseButtonState.Pressed Then
+                point = e.GetPosition(Nothing)
+                Dim grid As GridControl = TryCast(sender, GridControl)
+                Dim info As GridViewHitInfoBase
+                If grid.View.GetType() Is GetType(TableView) Then
+                    Dim view As TableView = TryCast(grid.View, TableView)
+                    info = view.CalcHitInfo(TryCast(e.OriginalSource, DependencyObject))
+                Else
+                    Dim view As CardView = TryCast(grid.View, CardView)
+                    info = view.CalcHitInfo(TryCast(e.OriginalSource, DependencyObject))
+                End If
 
-			If Not rect.Contains(mousePos) Then
-				Dim dragData As New DataObject("GridToLC", (TryCast(view.GetRowElementByRowHandle(handler).DataContext, RowData)).Row)
-				SetToZero()
-				DragDrop.DoDragDrop(grid, dragData, DragDropEffects.Copy)
-			End If
-		End Sub
+                If info.RowHandle <> DataControlBase.InvalidRowHandle Then
+                    handler = info.RowHandle
+                    e.Handled = True
+                End If
+            End If
+        End Sub
 
-		Private Sub tileLayoutControl1_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
-			If e.Data.GetDataPresent("GridToLC") Then
-				Dim obj As ExampleObject = TryCast(e.Data.GetData("GridToLC"), ExampleObject)
-				Dim tileLayout As TileLayoutControl = TryCast(sender, TileLayoutControl)
-				TryCast(tileLayout.ItemsSource, ObservableCollection(Of ExampleObject)).Add(obj)
-			End If
-		End Sub
+        Private Sub tileLayoutControl1_DragEnter(ByVal sender As Object, ByVal e As DragEventArgs)
+            If Not e.Data.GetDataPresent("GridToLC") OrElse sender Is e.Source Then
+                e.Effects = DragDropEffects.None
+            End If
+        End Sub
 
-		Private Sub gridControl1_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-			If e.LeftButton = MouseButtonState.Pressed Then
-				point = e.GetPosition(Nothing)
-				Dim grid As GridControl = TryCast(sender, GridControl)
-				Dim info As GridViewHitInfoBase
+'#End Region
+'#Region "TileLayoutToGrid"
+        Private Sub tileLayoutControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+            If e.LeftButton <> MouseButtonState.Pressed Then Return
+            Dim tileLayoutControl As TileLayoutControl = TryCast(sender, TileLayoutControl)
+            Dim mousePos As Point = e.GetPosition(Me.tileLayoutControl1)
+            Dim rect As Rect = New Rect(point.X - SystemParameters.MinimumHorizontalDragDistance, (point.Y - CInt(SystemParameters.MinimumVerticalDragDistance)), SystemParameters.MinimumHorizontalDragDistance * 2, SystemParameters.MinimumVerticalDragDistance * 2)
+            If Not rect.Contains(mousePos) Then
+                Dim hitTest As IInputElement = tileLayoutControl.InputHitTest(mousePos)
+                Dim tile As Tile = TryCast(LayoutHelper.FindParentObject(Of Tile)(CType(hitTest, DependencyObject)), Tile)
+                If tile IsNot Nothing Then
+                    Dim dragData As DataObject = New DataObject("LCToGrid", tile.Content)
+                    SetToZero()
+                    DragDrop.DoDragDrop(tileLayoutControl, dragData, DragDropEffects.Copy)
+                End If
+            End If
+        End Sub
 
-				If grid.View.GetType() Is GetType(TableView) Then
-					Dim view As TableView = TryCast(grid.View, TableView)
-					info = view.CalcHitInfo(TryCast(e.OriginalSource, DependencyObject))
-				Else
-					Dim view As CardView = TryCast(grid.View, CardView)
-					info = view.CalcHitInfo(TryCast(e.OriginalSource, DependencyObject))
-				End If
+        Private Sub tileLayoutControl1_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            If e.LeftButton = MouseButtonState.Pressed Then
+                point = e.GetPosition(Me.tileLayoutControl1)
+            End If
+        End Sub
 
-				If info.RowHandle <> GridControl.InvalidRowHandle Then
-					handler = info.RowHandle
-					e.Handled = True
-				End If
-			End If
+        Private Sub gridControl1_DragEnter(ByVal sender As Object, ByVal e As DragEventArgs)
+            If Not e.Data.GetDataPresent("LCToGrid") OrElse sender Is TryCast(e.Source, Tile) Then
+                e.Effects = DragDropEffects.None
+            End If
+        End Sub
 
-		End Sub
-
-		Private Sub tileLayoutControl1_DragEnter(ByVal sender As Object, ByVal e As DragEventArgs)
-			If (Not e.Data.GetDataPresent("GridToLC")) OrElse sender Is e.Source Then
-				e.Effects = DragDropEffects.None
-			End If
-		End Sub
-		#End Region
-		#Region "TileLayoutToGrid"
-
-		Private Sub tileLayoutControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
-			If e.LeftButton <> MouseButtonState.Pressed Then
-				Return
-			End If
-			Dim tileLayoutControl As TileLayoutControl = TryCast(sender, TileLayoutControl)
-			Dim mousePos As Point = e.GetPosition(tileLayoutControl1)
-
-			Dim rect As New Rect((point.X - SystemParameters.MinimumHorizontalDragDistance), (point.Y - CInt(Math.Truncate(SystemParameters.MinimumVerticalDragDistance))), SystemParameters.MinimumHorizontalDragDistance * 2, SystemParameters.MinimumVerticalDragDistance * 2)
-
-			If Not rect.Contains(mousePos) Then
-				Dim hitTest As IInputElement = tileLayoutControl.InputHitTest(mousePos)
-				Dim tile As Tile = TryCast(LayoutHelper.FindParentObject(Of Tile)(CType(hitTest, DependencyObject)), Tile)
-				If tile IsNot Nothing Then
-					Dim dragData As New DataObject("LCToGrid", tile.Content)
-					SetToZero()
-					DragDrop.DoDragDrop(tileLayoutControl, dragData, DragDropEffects.Copy)
-				End If
-			End If
-		End Sub
-
-		Private Sub tileLayoutControl1_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-			If e.LeftButton = MouseButtonState.Pressed Then
-				point = e.GetPosition(tileLayoutControl1)
-			End If
-		End Sub
-
-		Private Sub gridControl1_DragEnter(ByVal sender As Object, ByVal e As DragEventArgs)
-			If (Not e.Data.GetDataPresent("LCToGrid")) OrElse sender Is TryCast(e.Source, Tile) Then
-				e.Effects = DragDropEffects.None
-			End If
-		End Sub
-
-		Private Sub gridControl1_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
-			If e.Data.GetDataPresent("LCToGrid") Then
-				Dim obj As ExampleObject = TryCast(e.Data.GetData("LCToGrid"), ExampleObject)
-				Dim grid As GridControl = TryCast(sender, GridControl)
-				TryCast(grid.ItemsSource, ObservableCollection(Of ExampleObject)).Add(obj)
-			End If
-		End Sub
-		#End Region
-
-
-
-	End Class
+        Private Sub gridControl1_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
+            If e.Data.GetDataPresent("LCToGrid") Then
+                Dim obj As ExampleObject = TryCast(e.Data.GetData("LCToGrid"), ExampleObject)
+                Dim grid As GridControl = TryCast(sender, GridControl)
+                TryCast(grid.ItemsSource, ObservableCollection(Of ExampleObject)).Add(obj)
+            End If
+        End Sub
+'#End Region
+    End Class
 End Namespace
